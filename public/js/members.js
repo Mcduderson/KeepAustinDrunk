ac0fabafe1caddbef871c5ecccadcc4a9544efe8
Michelle Moon <robbins.michelle@ymail.com>
2020-11-02 16:50:16 -0600

    one like

--- public/js/members.js
+++ public/js/members.js
@@ -1,10 +1,10 @@
-// $(document).ready(function() {
-//   // This file just does a GET request to figure out which user is logged in
-//   // and updates the HTML on the page
-//   $.get("/api/user_data").then(function(data) {
-//     $(".member-name").text(data.email);
-//   });
-// });
+$(document).ready(function() {
+  // This file just does a GET request to figure out which user is logged in
+  // and updates the HTML on the page
+  $.get("/api/user_data").then(function(data) {
+    $(".member-name").text(data.email);
+  });
+});
 
 $(document).on('click', '#post-button', function (event) {
   
@@ -28,8 +28,10 @@ $(document).on('click', '#post-button', function (event) {
 
 // Click event to increase number with like button
 $(document).on('click', '#likes', function (event) {
+var likeBttn = $("#likes");
+likeBttn.text(1);
 
-}
+});
 
 
 // input box displays username, time stamp(date), and location of hh 
