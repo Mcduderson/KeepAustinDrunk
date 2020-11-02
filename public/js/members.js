eb5dfd1e09d461916fd512268a855d0076db80b8
Michelle Moon <robbins.michelle@ymail.com>
2020-11-02 16:39:21 -0600

    dynamically creating card content

--- public/js/members.js
+++ public/js/members.js
@@ -1,10 +1,34 @@
-$(document).ready(function() {
-  // This file just does a GET request to figure out which user is logged in
-  // and updates the HTML on the page
-  $.get("/api/user_data").then(function(data) {
-    $(".member-name").text(data.email);
-  });
+// $(document).ready(function() {
+//   // This file just does a GET request to figure out which user is logged in
+//   // and updates the HTML on the page
+//   $.get("/api/user_data").then(function(data) {
+//     $(".member-name").text(data.email);
+//   });
+// });
+
+$(document).on('click', '#post-button', function (event) {
+  
+ var textPost = $("#textArea").val();
+  // send the value to the server
+ var location = $("#bar").val();
+ var postEl = $(".container");
+ var cardEl = $("<div>").addClass("card w-75");
+  var cardBody = $("<div>").addClass("card-body");
+  var cardTitle = $("<h5>").addClass("card-title").text(location);
+  var cardText = $("<p>").addClass("card-text").text(textPost);
+  var cardButton = $("<button>").addClass("btn btn-outline-warning");
+  postEl.append(cardEl);
+  cardEl.append(cardBody);
+  cardBody.append(cardTitle);
+  cardBody.append(cardText);
+  cardBody.append(cardButton);
+
 });
+
+
+
+// on click empty form and create new post on page
+
 
 // input box displays username, time stamp(date), and location of hh 
 // db posts , username, created_date, location, body
